299061ec4338c6a903768a2fef8cd942e176d8f8
Ken Luzuriaga <ken.luzuriaga@trescloud.com>
2025-12-11 23:28:07 -0500

    add: dockerfiles

---
+++ orders-api/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST || 'db',
+  user: process.env.DB_USER || 'user',
+  password: process.env.DB_PASS || 'password',
+  database: process.env.DB_NAME || 'orders_db',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
